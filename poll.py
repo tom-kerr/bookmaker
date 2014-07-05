e681489412e8aaf1765aebf6108a941f41ca92ff
reklaklislaw <thomkerr@gmail.com>
2014-07-04 22:52:49 -0400

    Revert "remove abillity of thread/exception polls to halt under certain conditions"
    This reverts commit 32d17a444bfa938bc37e2c7ddc085135de89fe8a.

--- poll.py
+++ poll.py
@@ -74,6 +74,10 @@ class ShellPolls(BasePolls):
     def _thread_poll(self):
         while True:
             time.sleep(1.0)
+            if (not self._should_poll or 
+                not self.ProcessHandler._are_active_processes()):
+                self._is_polling_threads = False
+                break
             self.ProcessHandler._clear_inactive()
             self.ProcessHandler._submit_waiting()
             
@@ -86,6 +90,10 @@ class ShellPolls(BasePolls):
     def _exception_poll(self):
         while True:
             time.sleep(1.0)
+            if (not self._should_poll or 
+                not self.ProcessHandler._are_active_processes()):
+                self._is_polling_exceptions = False
+                break
             try:
                 pid, traceback = self.ProcessHandler._exception_queue.get_nowait()
             except Empty:
@@ -116,6 +124,11 @@ class GUIPolls(BasePolls):
             self._is_polling_threads = True
         
     def _thread_poll(self):
+        if (not self._should_poll or 
+            not self.ProcessHandler._are_active_processes()):
+            self._is_polling_threads = False
+            self._should_poll = False
+            return False
         self.ProcessHandler._clear_inactive()
         self.ProcessHandler._submit_waiting()
         return True
@@ -126,6 +139,11 @@ class GUIPolls(BasePolls):
             self._is_polling_exceptions = True
 
     def _exception_poll(self):
+        if (not self._should_poll or 
+            not self.ProcessHandler._are_active_processes()):
+            self._is_polling_exceptions = False
+            self._should_poll = False
+            return False
         try:
             pid, traceback = self.ProcessHandler._exception_queue.get_nowait()
         except Empty:
@@ -137,4 +155,5 @@ class GUIPolls(BasePolls):
             self.ProcessHandler.finish(identifier)
             ca.dialog(message=msg)
             return True
+            #raise Exception(msg)
             
