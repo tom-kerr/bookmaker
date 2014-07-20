6c7e47ddaf561601e1401345e8dcfe340f7ecb9a
reklaklislaw <thomkerr@gmail.com>
2014-07-20 17:00:11 -0400

    add methods for calling custom hooks

--- events.py
+++ events.py
@@ -6,10 +6,15 @@ class OnEvents(object):
         self.failure_hooks = []
         self.exit_hooks = []
 
-    def on_success(self, **kwargs):
+    def on_success(self, *args, **kwargs):
         pass
 
-    def on_failure(self, exception=None, **kwargs):
+    def call_success_hooks(self, *args, **kwargs):
+        while self.success_hooks:
+            hook = self.success_hooks.pop()
+            hook(*args, **kwargs)
+
+    def on_failure(self, *args, exception=None, **kwargs):
         runtimeerror = RuntimeError('An error occurred executing ' + 
                                     self.__class__.__name__)
         if exception:
@@ -17,40 +22,45 @@ class OnEvents(object):
         else:
             raise runtimeerror
 
-    def on_exit(self, **kwargs):
+    def call_failure_hooks(self, *args, **kwargs):
+        while self.failure_hooks:
+            hook = self.failure_hooks.pop()
+            hook(*args, **kwargs)
+
+    def on_exit(self, *args, **kwargs):
         pass
+
+    def call_exit_hooks(self, *args, **kwargs):
+        while self.exit_hooks:
+            hook = self.exit_hooks.pop()
+            hook(*args, **kwargs)
 
     def event_trigger(self, event, *args, **kwargs):
         try:
             if event == True:
                 try:
-                    self.on_success(**kwargs)
+                    self.on_success(*args, **kwargs)
                 except (Exception, BaseException):
                     raise
                 finally:
-                    while self.success_hooks:
-                        hook = self.success_hooks.pop()
-                        hook(*args, **kwargs)
+                    self.call_success_hooks(*args, **kwargs)
             elif event == False:
                 try:
-                    while self.failure_hooks:
-                        hook = self.failure_hooks.pop()
-                        hook(*args, **kwargs)
+                    self.call_failure_hooks(*args, **kwargs)
                 except (Exception, BaseException):
                     raise
                 finally:
-                    self.on_failure()
+                    self.on_failure(*args, **kwargs)
         except:
             raise
         finally:
             try:
-                self.on_exit(**kwargs)
+                self.on_exit(*args, **kwargs)
             except (Exception, BaseException):
                 raise
             finally:
-                while self.exit_hooks:
-                    hook = self.exit_hooks.pop()
-                    hook(*args, **kwargs)
+                self.call_exit_hooks(*args, **kwargs)
+
     
 def handle_events(f):
     def on_event(self, *args, **kwargs):
@@ -58,29 +68,24 @@ def handle_events(f):
             f(self, *args, **kwargs)
         except (Exception, BaseException) as e:
             try:
-                while self.failure_hooks:
-                    hook = self.failure_hooks.pop()
-                    hook(*args, **kwargs)
+                self.call_failure_hooks(*args, **kwargs)
             except (Exception, BaseException):
                 raise
             finally:
-                self.on_failure(exception=e)
+                self.on_failure(*args, exception=e, **kwargs)
         else:
             try:
-                self.on_success(**kwargs)
+                self.on_success(*args, **kwargs)
             except (Exception, BaseException):
                 raise
             finally:
-                while self.success_hooks:
-                    hook = self.success_hooks.pop()
-                    hook(*args, **kwargs)
+                self.call_success_hooks(*args, **kwargs)
         finally:
             try:
-                self.on_exit(**kwargs)
+                self.on_exit(*args, **kwargs)
             except (Exception, BaseException):
                 raise
             finally:
-                while self.exit_hooks:
-                    hook = self.exit_hooks.pop()
-                    hook(*args, **kwargs)
+                self.call_exit_hooks(*args, **kwargs)
     return on_event
+
